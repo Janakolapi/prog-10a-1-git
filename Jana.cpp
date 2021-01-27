ed08b2bf5a0cf9d939d89132955584498f3baafe
Jana <77438400+Janakolapi@users.noreply.github.com>
2021-01-27 22:16:13 +0200

    Add files via upload
    Jana Molotkova

---
+++ Jana.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Ievadi nummuru: ";
+    cin >> number;
+
+    cout << "nummurs- " << number;
+    return 0;
+}
